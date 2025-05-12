d9cf35c85b924c3b96dd50dc490a704145364059
codelavkush <mauryalavkush19@gmail.com>
2025-05-12 10:25:20 +0530

    changes in settings and env

--- GreenBloom/GreenBloom/settings.py
+++ GreenBloom/GreenBloom/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 
 load_dotenv()
 ENVIRONMENT = os.getenv('ENVIRONMENT')
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

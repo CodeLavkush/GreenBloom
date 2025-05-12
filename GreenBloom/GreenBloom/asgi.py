4a0596b8afe7be395aeb6380bef50d6d1cfc78c1
codelavkush <mauryalavkush19@gmail.com>
2025-05-12 10:03:10 +0530

    updated path in wsgi & asgi

--- GreenBloom/GreenBloom/asgi.py
+++ GreenBloom/GreenBloom/asgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
+import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 from django.core.asgi import get_asgi_application
 
